3ff3011dcddfad6d8de70a4636ac379143672f91
furyseldom <44933314+furyseldom@users.noreply.github.com>
2019-05-01 17:46:09 +0300

    Update scr.js

--- js/scr.js
+++ js/scr.js
@@ -1,40 +1,44 @@
     $(document).ready(function () {
+        var blueLink = $(".blue-link");
+        var greyLink = $(".grey-link");
+        var blackLink = $(".black-link")
+        var footerLink = $(".footer-link");
         /*Код для синих ссылок в навбарах наверху*/
-        $(".blue-link").css('color', '#0091d9');
-        $(".blue-link").mouseover(function () {
+        $(blueLink).css('color', '#0091d9');
+        $(blueLink).mouseover(function () {
             $(this).css('color', 'red');
         });
 
-        $(".blue-link").mouseout(function () {
+        $(blueLink).mouseout(function () {
             $(this).css('color', '#0091d9');
         });
         /*Конец*/  
         /*Серые ссылки*/
-        $(".grey-link").css('color', '#a1a1a1');
-        $(".grey-link").mouseover(function () {
+        $(greyLink).css('color', '#a1a1a1');
+        $(greyLink).mouseover(function () {
             $(this).css('color', 'red');
         });
 
-        $(".grey-link").mouseout(function () {
+        $(greyLink).mouseout(function () {
             $(this).css('color', '#a1a1a1');
         });
         /*Конец*/
         
         /*Чёрные ссылки*/
-        $(".black-link").css('color', '#000');
-        $(".black-link").mouseover(function () {
+        $(blackLink).css('color', '#000');
+        $(blackLink).mouseover(function () {
             $(this).css('color', 'red');
         });
-        $(".black-link").mouseout(function () {
+        $(blackLink).mouseout(function () {
             $(this).css('color', '#000');
         });
           /*Конец*/
         
-        $(".footer-link").css('color', '#858585');
-           $(".footer-link").mouseover(function () {
+        $(footerLink).css('color', '#858585');
+           $(footerLink).mouseover(function () {
             $(this).css('color', 'red');
         });
-        $(".footer-link").mouseout(function () {
+        $(footerLink).mouseout(function () {
             $(this).css('color', '#858585');
         });
     });
