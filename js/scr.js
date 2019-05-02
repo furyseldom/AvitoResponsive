5618f7b52fb0f3dd90ef2e28f98309d1e33de69b
furyseldom <44933314+furyseldom@users.noreply.github.com>
2019-05-02 15:42:56 +0300

    Add files via upload

---
+++ js/scr.js
@@ -0,0 +1,55 @@
+    $(document).ready(function () {
+        var blueLink = $(".blue-link");
+        var greyLink = $(".grey-link");
+        var blackLink = $(".black-link")
+        var footerLink = $(".footer-link");
+        
+        if($(Document).width() > 800 ){
+            $(".facts-section").addClass("container");
+
+        }
+        if($(Document).width() < 800 ){
+            $(".facts-section").addClass("container-fluid");
+        
+        }
+        /*Код для синих ссылок в навбарах наверху*/
+        $(blueLink).css('color', '#0091d9');
+        $(blueLink).mouseover(function () {
+            $(this).css('color', 'red');
+        });
+
+        $(blueLink).mouseout(function () {
+            $(this).css('color', '#0091d9');
+        });
+        /*Конец*/  
+        /*Серые ссылки*/
+        $(greyLink).css('color', '#a1a1a1');
+        $(greyLink).mouseover(function () {
+            $(this).css('color', 'red');
+        });
+
+        $(greyLink).mouseout(function () {
+            $(this).css('color', '#a1a1a1');
+        });
+        /*Конец*/
+        
+        /*Чёрные ссылки*/
+        $(blackLink).css('color', '#000');
+        $(blackLink).mouseover(function () {
+            $(this).css('color', 'red');
+        });
+        $(blackLink).mouseout(function () {
+            $(this).css('color', '#000');
+        });
+          /*Конец*/
+        
+        $(footerLink).css('color', '#858585');
+           $(footerLink).mouseover(function () {
+            $(this).css('color', 'red');
+        });
+        $(footerLink).mouseout(function () {
+            $(this).css('color', '#858585');
+        });
+        
+    });
+      
